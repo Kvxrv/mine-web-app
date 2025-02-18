817c476ccaf8f6217b063dc90a4591d7997e9f5c
Karthika <karthikavprakash@gmail.com>
2025-02-19 01:40:53 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello, Render!"
 
+@app.route('/about')
+def about():
+    return "This is the About Page!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
